ca53792b8f0dbbf8134425578740c3578b23544d
Ainan-code <ainanhassan123@gmail.com>
2024-10-28 19:20:56 +0300

    ui design implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import Signup from './pages/signup/SignupPage';
 import Login from './pages/login/LoginPage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 
 function App() {
@@ -10,11 +14,15 @@ function App() {
 
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
      <Routes>
       <Route path='/' element={<Homepage/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/notifications' element={<NotificationPage/>}/>
+      <Route path='/profile/:username' element={<ProfilePage/>}/>
      </Routes>
+     <RightPanel/>
     </div>
   )
 }
